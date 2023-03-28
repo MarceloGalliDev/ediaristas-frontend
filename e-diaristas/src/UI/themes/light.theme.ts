cf63f5bcedd86b8ca39997f2e5bcf6d296e24569
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-03-27 22:33:13 -0300

    update
    update

--- e-diaristas/src/UI/themes/light.theme.ts
+++ e-diaristas/src/UI/themes/light.theme.ts
@@ -16,8 +16,22 @@ const theme = createTheme({
       primary: "#707070",
       secondary: "#9B9B9B"
     },
-    error: {},
-    warning: {},
+    error: {
+      main: "#FC3C00"
+    },
+    warning: {
+      main: "#FCA600"
+    },
+    success: {
+      main: "#00D34D"
+    },
+    grey: {
+      50: "#FAFAFA",
+      100: "#F0F0F0",
+      200: "#D7D9DD",
+      300: "#C4C4C4",
+      400: "#9B9B9B"
+    },
   },
 })
 
