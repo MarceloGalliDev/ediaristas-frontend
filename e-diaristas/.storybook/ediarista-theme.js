02f695248e5e2e39abfa19467322b3fc15c27a29
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-04-02 20:59:37 -0300

    update
    update

--- e-diaristas/.storybook/ediarista-theme.js
+++ e-diaristas/.storybook/ediarista-theme.js
@@ -2,5 +2,6 @@ import { create } from '@storybook/theming'
 
 export default create({
   brandTitle: 'e-diaristas',
-  brandImage: './img/lo'
+  brandImage: '../public/img/logos/logo.svg',
+  brandUrl: 'https://treinaweb.com.br'
 })
--- e-diaristas/.storybook/main.js
+++ e-diaristas/.storybook/main.js
@@ -1,11 +1,20 @@
 module.exports = {
-  "stories": ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
-  "framework": {
-    name: "@storybook/nextjs",
-    options: {}
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm"
+  ],
+  framework: {
+    "name": "@storybook/nextjs",
+    "options": {}
   },
   docs: {
-    autodocs: true
+    "autodocs": true,
+  },
+  staticDirs: ["../public"],
+  core: {
+    "builder": "@storybook/builder-webpack5"
   }
 };
