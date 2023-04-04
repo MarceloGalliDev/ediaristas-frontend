6325634f04090799a1520848ccb74918175bf512
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-04-03 21:21:18 -0300

    update
    update

--- e-diaristas/.storybook/main.js
+++ e-diaristas/.storybook/main.js
@@ -1,4 +1,16 @@
+const path = require('path')
+
+const toPath = (_path) => path.join(process.cwd(), _path)
+
 module.exports = {
+  webpackFinal: async (config) => {
+    config.resolve.modules.push(toPath('src')),
+    config.resolve.alias['@emotion/core'] = toPath('node_modules/@emotion/react')
+    config.resolve.alias['@emotion/styled'] = toPath('node_modules/@emotion/styled')
+    config.resolve.alias['emotion-theming'] = toPath('node_modules/@emotion/react')
+
+    return config;
+  },
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
--- e-diaristas/src/stories/Button.stories.tsx
+++ e-diaristas/src/stories/Button.stories.tsx
@@ -1,41 +1,49 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-
-import { Button } from './Button';
-
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-export default {
-  title: 'Example/Button',
-  component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} as ComponentMeta<typeof Button>;
-
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
-
-export const Primary = Template.bind({});
+// More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from './Button';
+import { Button as MuiButton } from '@mui/material'
+
+
+const meta: Meta<typeof Button> = {
+  title: 'Button',
+  component: Button,
+  //args:{children: "click me!"}, aqui passo uma parametro se existir no ButtonProps
+  //argTypes: {size: { control: "select" }}. aqui mudo o modelo dos botões do storybook
+  //argTypes: {size: { control: "select" }, children: {options: ['Italic'], mapping: {Italic: <em>Italic text</em>}}}.
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+  args: {
+    primary: true,
+    label: 'Button primary'
+  }
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
+export const Secondary: Story = {
+  args: {
+    primary: false,
+    label: 'Button secondary'
+  }
 };
 
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+export const Large: Story = {
+  args: {
+    size: 'large',
+    label: 'Button',
+  }
 };
+
+export const Teste = {
+  render: (args: any) => <MuiButton {...args}>Click</MuiButton>,
+  args: {
+    variant: 'contained'
+  }
+}
