78ae0d5cebe23915e21f06aedacd702597149779
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-04-06 22:20:52 -0300

    update
    update

--- e-diaristas/src/UI/components/surfaces/Footer/Footer.styled.tsx
+++ e-diaristas/src/UI/components/surfaces/Footer/Footer.styled.tsx
@@ -1,5 +1,31 @@
+import { Container, ListItem, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import { PropsWithChildren } from 'react';
 
 export const FooterContainer = styled('footer')`
   background-color: ${({ theme }) => theme.palette.primary.main};
+  color: ${({ theme }) => theme.palette.primary.contrastText};
+  padding: ${({ theme }) => theme.spacing(4,0)};
+`;
+
+export const FooterGrid = styled(Container)`
+  display: flex;
+  justify-content: space-between;
+  gap: ${({ theme }) => theme.spacing(2)};
+  flex-wrap: wrap;
+`;
+
+
+export const FooterListItem = styled((props: PropsWithChildren) => (
+  <ListItem disableGutters {...props} />
+))``;
+
+export const FooterTitle = styled((props: PropsWithChildren) => (
+  <Typography variant={"body2"} component={"h2"} {...props}/>
+))`
+  font-weight: bold;
+`;// aqui usamos uma função para retorna o typografy, e usamos as props para ter acesso a elas, usamos do tipo children, props com filho
+
+export const SocialContainer = styled("div")`
+
 `;
