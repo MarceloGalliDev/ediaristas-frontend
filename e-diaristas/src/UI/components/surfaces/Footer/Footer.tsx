79fe537723f2710f84db5b2b2a86c77b50ab2c0a
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-04-07 20:22:19 -0300

    update
    update

--- e-diaristas/src/UI/components/surfaces/Footer/Footer.tsx
+++ e-diaristas/src/UI/components/surfaces/Footer/Footer.tsx
@@ -19,31 +19,37 @@ const Footer = () => {
           <FooterTitle>Menu</FooterTitle>
           <List>
             <FooterListItem>
-              <Link href="/encontrar-diarista" mui={{color: 'inherit', variant:'body2'}}>
+              <Link
+                href="/encontrar-diarista"
+                mui={{ color: "inherit", variant: "body2" }}
+              >
                 Encontrar um(a) diarista
               </Link>
             </FooterListItem>
             <FooterListItem>
-              <Link href="/encontrar-diarista" mui={{color: 'inherit', variant:'body2'}}>
+              <Link
+                href="/encontrar-diarista"
+                mui={{ color: "inherit", variant: "body2" }}
+              >
                 Seja um(a) diarista
               </Link>
             </FooterListItem>
             <FooterListItem>
-              <Link href="/" mui={{color: 'inherit', variant:'body2'}}>
+              <Link href="/" mui={{ color: "inherit", variant: "body2" }}>
                 Por que usar o e-diarista
               </Link>
             </FooterListItem>
             <FooterListItem>
-              <Link href="/" mui={{color: 'inherit', variant:'body2'}}>
+              <Link href="/" mui={{ color: "inherit", variant: "body2" }}>
                 Principais Dúvidas
               </Link>
             </FooterListItem>
           </List>
         </div>
-        <Box sx={{maxWidth: '400px'}}>
+        <Box sx={{ maxWidth: "400px" }}>
           <FooterTitle>Quem somos</FooterTitle>
-          <Typography variant={'body2'} sx={{ mt: 2 }}>
-            E-Diaristas te ajuda a encontrar um profissional perfeito para 
+          <Typography variant={"body2"} sx={{ mt: 2 }}>
+            E-Diaristas te ajuda a encontrar um profissional perfeito para
             realizar a limpeza da sua casa. Garantimos o(a) melhor profissional
             com total segurança e praticidade! São milhares de clientes
             satisfeitos por todo o país.
@@ -54,7 +60,7 @@ const Footer = () => {
             <FooterTitle>Baixe nossos aplicativos</FooterTitle>
             <AppList>
               <li>
-                <a 
+                <a
                   href="https://apps.apple.com/br/app"
                   target="_blank"
                   rel="noopener noreferrer"
@@ -63,7 +69,7 @@ const Footer = () => {
                 </a>
               </li>
               <li>
-                <a 
+                <a
                   href="https://play.google.com/store/apps"
                   target="_blank"
                   rel="noopener noreferrer"
@@ -77,7 +83,7 @@ const Footer = () => {
             <FooterSocialList>
               <FooterListItem>
                 <SocialButton>
-                  <i className="twf-facebook-f"/>
+                  <i className="twf-facebook-f" />
                 </SocialButton>
               </FooterListItem>
             </FooterSocialList>
@@ -85,7 +91,7 @@ const Footer = () => {
         </SocialContainer>
       </FooterGrid>
     </FooterContainer>
-  )
+  );
   };
 
 export default Footer;
