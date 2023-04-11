e9a5d16dea4d1ac7d6ac5e922288df5db872c16f
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-04-11 20:24:44 -0300

    update
    update

--- e-diaristas/src/UI/partials/index/_advantages.tsx
+++ e-diaristas/src/UI/partials/index/_advantages.tsx
@@ -2,6 +2,45 @@ import { Container, ListItem, ListItemAvatar } from "@mui/material";
 import { AvatarStyled, GradientBackground, ListItemTextStyled, ListStyle, SectionTitle } from "./_advantages.style";
 
 const Advantages = () => {
+  const advantegeList = [
+    {
+      icon: "twf-woman",
+      title: "Diversidade",
+      description: "São mais de 5 mil profissionais esperando por você."
+
+    },
+    {
+      icon: "twf-certificade",
+      title: "Confiabilidade",
+      description: "Todos profissionais aqui listados possuem verificação de segurança."
+
+    },
+    {
+      icon: "twf-search-2",
+      title: "Rastreabilidade",
+      description: "Acesse o histórico dos profissionais."
+
+    },
+    {
+      icon: "twf-frame-broken",
+      title: "Segurança",
+      description: "Seguro sobre qualquer possível dano."
+
+    },
+    {
+      icon: "twf-payment",
+      title: "Controle",
+      description: "Pagamento é realizado somente com o profissional em seu local de destino, com confirmação."
+
+    },
+    {
+      icon: "twf-broom-bucket",
+      title: "Experiência",
+      description: "Mais de 50 mil diarias realizadas."
+
+    },
+  ];
+
   return (
     <GradientBackground>
       <Container>
@@ -10,59 +49,19 @@ const Advantages = () => {
         </SectionTitle>
         <ListStyle>
 
-          <ListItem>
-            <ListItemAvatar>
-              <AvatarStyled>
-                <i className="twf-certificate"/>
-              </AvatarStyled>
-            </ListItemAvatar>
-            <ListItemTextStyled primary={"Diversidade"} secondary={"Mais de 5 mil profissionais esperando por você."}/>
-          </ListItem>
-
-          <ListItem>
-            <ListItemAvatar>
-              <AvatarStyled>
-                <i className="twf-certificate"/>
-              </AvatarStyled>
-            </ListItemAvatar>
-            <ListItemTextStyled primary={"Diversidade"} secondary={"Mais de 5 mil profissionais esperando por você."}/>
-          </ListItem>
-
-          <ListItem>
-            <ListItemAvatar>
-              <AvatarStyled>
-                <i className="twf-certificate"/>
-              </AvatarStyled>
-            </ListItemAvatar>
-            <ListItemTextStyled primary={"Diversidade"} secondary={"Mais de 5 mil profissionais esperando por você."}/>
-          </ListItem>
-
-          <ListItem>
-            <ListItemAvatar>
-              <AvatarStyled>
-                <i className="twf-certificate"/>
-              </AvatarStyled>
-            </ListItemAvatar>
-            <ListItemTextStyled primary={"Diversidade"} secondary={"Mais de 5 mil profissionais esperando por você."}/>
-          </ListItem>
-
-          <ListItem>
-            <ListItemAvatar>
-              <AvatarStyled>
-                <i className="twf-certificate"/>
-              </AvatarStyled>
-            </ListItemAvatar>
-            <ListItemTextStyled primary={"Diversidade"} secondary={"Mais de 5 mil profissionais esperando por você."}/>
-          </ListItem>
-
-          <ListItem>
-            <ListItemAvatar>
-              <AvatarStyled>
-                <i className="twf-certificate"/>
-              </AvatarStyled>
-            </ListItemAvatar>
-            <ListItemTextStyled primary={"Diversidade"} secondary={"Mais de 5 mil profissionais esperando por você."}/>
-          </ListItem>
+          {advantegeList.map((item, index) => {
+            return (
+              <ListItem key={index}>
+                <ListItemAvatar>
+                  <AvatarStyled>
+                    <i className="twf-certificate"/>
+                  </AvatarStyled>
+                </ListItemAvatar>
+                <ListItemTextStyled primary={"Diversidade"} secondary={"Mais de 5 mil profissionais esperando por você."}/>
+              </ListItem>
+            )
+          })}
+          
 
         </ListStyle>
       </Container>
