774e9f7254febbd2fb76b7ac445bf5f8b76756bc
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-04-11 22:42:38 -0300

    update
    update

--- e-diaristas/src/UI/partials/index/_advantages.tsx
+++ e-diaristas/src/UI/partials/index/_advantages.tsx
@@ -4,32 +4,38 @@ import { AvatarStyled, GradientBackground, ListDivider, ListItemTextStyled, List
 const Advantages = () => {
   const advantegeList = [
     {
+      id: 1,
       icon: "twf-woman",
       title: "Diversidade",
       description: "São mais de 5 mil profissionais esperando por você.",
     },
     {
+      id: 2,
       icon: "twf-certificate",
       title: "Confiabilidade",
       description: "Todos profissionais aqui listados possuem verificação de segurança.",
     },
     {
+      id: 3,
       icon: "twf-search-2",
       title: "Rastreabilidade",
       description: "Acesse o histórico dos profissionais.",
     },
     {
+      id: 4,
       icon: "twf-frame-broken",
       title: "Segurança",
       description: "Seguro sobre qualquer possível dano.",
     },
     {
+      id: 5,
       icon: "twf-payment",
       title: "Controle",
       description:
         "Pagamento é realizado somente com o profissional em seu local de destino, com confirmação.",
     },
     {
+      id: 6,
       icon: "twf-broom-bucket",
       title: "Experiência",
       description: "Mais de 50 mil diarias realizadas.",
@@ -48,7 +54,7 @@ const Advantages = () => {
             return (
               <>
                 <ListDivider></ListDivider>
-                <ListItem key={index}>
+                <ListItem key={item.id}>
                   <ListItemAvatar>
                     <AvatarStyled>
                       <i className={item.icon}/>
