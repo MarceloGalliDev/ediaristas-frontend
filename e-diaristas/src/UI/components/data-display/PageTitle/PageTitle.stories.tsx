cae1d3c0030e54cd451614499ae2f02eb8f81735
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-04-24 22:32:56 -0300

    update
    update

--- e-diaristas/src/UI/components/data-display/PageTitle/PageTitle.stories.tsx
+++ e-diaristas/src/UI/components/data-display/PageTitle/PageTitle.stories.tsx
@@ -7,6 +7,16 @@ type Story = StoryObj<typeof PageTitle>;
 const meta: Meta<typeof PageTitle> = {
   title: "data-display/PageTitle",
   component: PageTitle,
+  argTypes: {
+    title: {
+      description: "Titulo da Página, contém MediumDevices",
+      defaultValue: "Conheça os Profissionais",
+    },
+    subtitle: {
+      description: "Subtitulo da Página, contém MediumDevices",
+      defaultValue: "Preencha seu endereço e veja todos os profissionais da sua localidade",
+    },
+  },
 };
 
 export default meta;
@@ -14,7 +24,7 @@ export default meta;
 export const ComponentStory: Story = {
   render: (args: any) => <PageTitle {...args} />,
   args: {
-    title: 'Conheça os Profissionais',
-    subtitle: 'Preencha seu endereço e veja todos os profissionais da sua localidade',
+    title: "Conheça os Profissionais",
+    subtitle: "Preencha seu endereço e veja todos os profissionais da sua localidade",
   },
 };
--- e-diaristas/src/UI/components/data-display/PageTitle/PageTitle.styled.tsx
+++ e-diaristas/src/UI/components/data-display/PageTitle/PageTitle.styled.tsx
@@ -7,8 +7,24 @@ export const PageTitleContainer = styled('div')`
 
 export const PageTitleStyled = styled('h2')`
   font-family: ${({ theme }) => theme.typography.fontFamily};
+  font-size: ${({ theme }) => theme.typography.h6.fontSize};
+  font-weight: 600;
+  margin: 0;
+  color: ${({ theme }) => theme.palette.primary.main};
+
+  ${({ theme }) => theme.breakpoints.down("md")} {
+    font-size: ${({ theme }) => theme.typography.body1.fontSize};
+  }
 `;
 
 export const PageSubtitleStyled = styled("h3")`
   font-family: ${({ theme }) => theme.typography.fontFamily};
+  margin: ${({ theme }) => theme.spacing(1.5, 0)};
+  color: ${({ theme }) => theme.palette.text.primary};
+  font-weight: normal;
+  font-size: ${({ theme }) => theme.typography.body1.fontSize};
+
+  ${({ theme }) => theme.breakpoints.down("md")} {
+    font-size: ${({ theme }) => theme.typography.body2.fontSize};
+  }
 `;
