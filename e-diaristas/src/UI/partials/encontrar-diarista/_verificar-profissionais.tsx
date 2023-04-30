cf7445f22b358d4943e426c72e69661689e18f20
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-04-30 00:52:25 -0300

    update
    update

---
+++ e-diaristas/src/UI/partials/encontrar-diarista/_verificar-profissionais.tsx
@@ -0,0 +1,13 @@
+import React, { PropsWithChildren } from 'react';
+//import {  } from 'react';
+//import { ComponentName } from './_verificar-profissionais.styled'; 
+
+const VerificarProfissionais: React.FC<PropsWithChildren> = () => {
+  return (
+    <div>
+      <div>VerificarProfissionais</div>
+    </div>
+  );
+};
+
+export default VerificarProfissionais;
---
+++ e-diaristas/src/pages/encontrar-diarista.tsx
@@ -0,0 +1,12 @@
+import VerificarProfissionais from "@partials/encontrar-diarista/_verificar-profissionais";
+import type { NextPage } from "next";
+
+const Index: NextPage = () => {
+  return (
+    <VerificarProfissionais>
+      encontrar diarista
+    </VerificarProfissionais>
+  )
+}
+
+export default Index;
