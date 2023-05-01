26c4b34c0c688fba7ca0a7c85960f9eedd4db95c
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-05-01 20:40:42 -0300

    update
    update

--- e-diaristas/cypress/e2e/3-ediaristas/busca-cep.cy.js
+++ e-diaristas/cypress/e2e/3-ediaristas/busca-cep.cy.js
@@ -1,9 +1,22 @@
 context('Busca pelo CEP', () => {
   beforeEach(() => {
     cy.visit('/encontrar-diarista')
-  })
+  });
 
   it('botao desabilitado e habilitado', () => {
-    cy.get('.MuiOutlinedInput-input').type('123456')
+    cy.get('.MuiOutlinedInput-input').type('1234567')
+    
+    //aqui colocamos uma const para testar diversas vezes a mesma função do cypres
+    const button = cy.get('button').contains(/buscar/i)
+    //verificando se o botão está vazio
+    button.should('be.disabled')
+    
+    //usamos o clear() para limpar o cache e depois executar a próxima função
+    cy.get('.MuiOutlinedInput-input').clear().type('1234567')
+    button.should('not.be.disabled')
+  });
+
+  it('buscar cep', () => {
+    
   })
 })
