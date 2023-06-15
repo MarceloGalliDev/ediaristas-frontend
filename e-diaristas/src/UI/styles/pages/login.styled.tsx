d3b05ed6ce881b86942aa6258d435eb1988d25a2
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-06-14 21:29:52 -0300

    update
    update

--- e-diaristas/src/UI/styles/pages/login.styled.tsx
+++ e-diaristas/src/UI/styles/pages/login.styled.tsx
@@ -1,12 +1,14 @@
+import { Button } from '@mui/material';
 import { styled } from '@mui/material/styles';
 //import { } from '@mui/material/';
 //import { LoginProps } from './Login'
 
-export const ComponentName = styled('div')`
+export const LoginContainer = styled('div')`
   color: ${({ theme }) => theme.palette.text.secondary};
   background-color: ${({ theme }) => theme.palette.background.default};
   font-size: 12px;
-  text-align: right;
   padding: ${({ theme }) => theme.spacing(2, 0)};
   font-family: ${({ theme }) => theme.typography.fontFamily}
 `;
+
+export const LoginButton = styled(Button)``;
--- e-diaristas/src/pages/login.tsx
+++ e-diaristas/src/pages/login.tsx
@@ -1,17 +1,50 @@
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
+import { LoginButton, LoginContainer } from "@styles/pages/login.styled";
 import PageTitle from "UI/components/data-display/PageTitle/PageTitle";
 import SafeEnvironment from "UI/components/feedback/SafeEnvironment/SafeEnvironment";
-import type { NextPage } from "next";
+import { LoginForm } from "UI/components/inputs/UserForm/forms/LoginForm";
+import useLogin from "data/hook/pages/useLogin.page";
+import type { GetStaticProps, NextPage } from "next";
+import { FormProvider } from "react-hook-form";
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      title: 'login',
+    }
+  }
+}
 
 const Index: NextPage = () => {
+  const { formMethods } = useLogin()
   return (
-    <div>
+    <FormProvider {...formMethods}>
       <SafeEnvironment />
       <Container>
-        <PageTitle title={""} />
+        <PageTitle title={"Informe seu email e senha"} />
+        <LoginContainer
+          as={'form'}
+          onSubmit={formMethods.handleSubmit()}
+        >
+          <LoginForm />
+          <Typography
+            color={'error'}
+            align={"center"}
+          >
+            Erro
+          </Typography>
+          <LoginButton
+            variant={"contained"}
+            size={"large"}
+            color={"secondary"}
+            type={"submit"}
+          >
+            Entrar
+          </LoginButton>
+        </LoginContainer>
       </Container>
       
-    </div>
+    </FormProvider>
   )
 }
 
